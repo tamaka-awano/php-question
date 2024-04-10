4ba8cbb4d0a3336890f4f1c71244dd7083beacc2
awano tamaka <atamakapooh13@gmail.com>
2024-04-10 22:32:24 +0900

    php

---
+++ config/cities.php
@@ -0,0 +1,40 @@
+<?php
+$cities = [
+    ['mane' => 'シドニー',
+    'time_zone' => 'Australia/Sydney',
+    'img' => 'australia.jpg'
+],
+[
+    'mane' => '東京',
+    'time_zone' => 'Asia/Tokyo',
+    'img' => 'japan.jpg'
+],
+[
+    'mane' => '上海',
+    'time_zone' => 'Asia/Shanghai',
+    'img' => 'china.jpg'
+],
+[
+    'mane' => 'モスクワ',
+    'time_zone' => 'Europe/Moscow',
+    'img' => 'russia.jpg'
+],
+[
+    'mane' => 'ロンドン',
+    'time_zone' => 'Europe/London',
+    'img' => 'british.jpg'
+],
+[
+    'mane' => 'ヨハネスブルグ',
+    'time_zone' => 'Africa/Johannesburg',
+    'img' => 'south-africa.jpg'
+],
+[
+    'mane' => 'ニューヨーク',
+    'time_zone' => 'America/New_York',
+    'img' => 'america.jpg'
+]
+];
+/* echo('<pre>');
+var_dump($cities);
+echo('</pre>'); */
